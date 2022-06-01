7345675a59055a6262751834453d059fc20aefb0
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-01 16:15:54 +1000

    Add project files.

---
+++ DaisyDays/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using DaisyDays.Tools;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DaisyDays
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ImageSpliter mySplitter = new();
+            this.InitializeComponent();
+            mySplitter.SplitImage("b://new avatar.png", 4);
+        }
+
+
+    }
+}
---
+++ DaisyDays/Tools/ImageSpliter.cs
@@ -0,0 +1,80 @@
+﻿using System.Drawing;
+
+namespace DaisyDays.Tools
+{
+    public class ImageSpliter
+    {
+
+
+        public void SplitImage()
+        {
+
+
+
+            //Create a new Bitmap object from the specified file
+            Bitmap myBitmap = new Bitmap("b://847.jpg");
+
+            //store original format
+            System.Drawing.Imaging.PixelFormat format = myBitmap.PixelFormat;
+
+            //Get the original image's width and height
+            int width = myBitmap.Width;
+            int height = myBitmap.Height;
+
+
+            //Make a rectangle for the new image's dimensions
+            RectangleF cloneRect = new RectangleF(0, 0, myBitmap.Width / 2, myBitmap.Height / 2);
+            //Create a new Bitmap object based on the specified width and height
+            Bitmap myBitmap2 = myBitmap.Clone(cloneRect, format);
+
+
+
+
+            ////Create a Graphics object
+            //Graphics g = Graphics.FromImage(myBitmap2);
+
+            ////Draw the specified image onto the new Bitmap object
+            //g.DrawImage(myBitmap, 0, 0, myBitmap2.Width, myBitmap2.Height);
+
+            //Save the new image to the specified file
+            myBitmap2.Save("b://847_4.jpg");
+
+            //Dispose of the Graphics object
+            //         g.Dispose();
+
+            //Dispose of the Bitmap objects
+            myBitmap.Dispose();
+            myBitmap2.Dispose();
+        }
+
+        public void SplitImage(string selectedImagePath, int numberOfParts)
+        {
+            //Create a new bitmap object from the specified file.
+            Bitmap originalImage = new Bitmap(selectedImagePath);
+            //Store original format
+            System.Drawing.Imaging.PixelFormat format = originalImage.PixelFormat;
+            //Get dimensions of original.
+            int width = originalImage.Width;
+            int height = originalImage.Height;
+
+            //Calculate Width and Height of each split file.
+
+            int numberPerRow = (int)System.Math.Sqrt(numberOfParts);
+            float widthOfEachPart = (width / numberOfParts) * numberPerRow;
+            float heightOfEachPart = (height / numberOfParts) * numberPerRow;
+
+            //Make array for output image dimensions
+            RectangleF[,] outputRects = new RectangleF[numberPerRow, numberPerRow];
+            for (int r = 0; r < numberPerRow; r++)
+                for (int c = 0; c < numberPerRow; c++)
+                {
+                    outputRects[r, c] = new RectangleF(c * widthOfEachPart,
+                     r * heightOfEachPart, widthOfEachPart, heightOfEachPart);
+                    Bitmap splitImage = originalImage.Clone(outputRects[r, c], format);
+                    splitImage.Save($"{selectedImagePath}_r{r}c{c}.jpg");
+                }
+
+
+        }
+    }
+}
