2dcb86ec3633b1225f4b0a6c8ca648f9f782c854
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-03 14:40:04 +1000

    It actually works now... now for the UI

--- DaisyDays/Tools/ImageSpliter.cs
+++ DaisyDays/Tools/ImageSpliter.cs
@@ -9,7 +9,8 @@ namespace DaisyDays.Tools
 
         string outputFolder = "B://Output";
 
-        public string[][] SplitImage(string selectedImagePath, int numberOfParts)
+        public int SplitImage(string selectedImagePath, int numberOfParts)
+
         {
             //Create a new bitmap object from the specified file.
             Bitmap originalImage = new Bitmap(selectedImagePath);
@@ -27,53 +28,34 @@ namespace DaisyDays.Tools
             int height = originalImage.Height;
 
             //Calculate The number of Rows and Columns
-            //int regularRow = (width > height) ?
-            //    (int)Ceiling(Sqrt(numberOfParts)) :
-            //    (int)Floor(Sqrt(numberOfParts));
-            int regularRow = (int)Floor(Sqrt(numberOfParts));//This is an attempt to isolate a bug   - using this splits everything into three rows of
-                                                             //four image, plus one final one for the final part 13 instead of 17
-
-
+            int regularRow = (int)Ceiling(Sqrt(numberOfParts)); //This works without a bug
             int lastRow = numberOfParts % regularRow;
-            int numberOfRows = (numberOfParts - lastRow) / regularRow;
+            int numberOfRows = ((numberOfParts - lastRow) / regularRow) + 1;
 
 
 
 
             //Calculate Width and Height of each split file.
-
-            float heightOfEachPart = height / regularRow;
-
+            float heightOfEachPart = height / numberOfRows;
 
 
 
 
             int r;
-            var ResultArray = new string[regularRow][];
+            //var ResultArray = new string[numberOfRows + 1][];
             lastRow = (lastRow == 0) ? regularRow : lastRow;
 
             //for (r = 0; r < regularRow - 1; r++)
             for (r = 0; r < numberOfRows - 1; r++)
                 GenerateRowOfPictures(r, regularRow);
             GenerateRowOfPictures(r, lastRow);
-            return ResultArray;
-
-
-            //for (int c = 0; c < regularRow; c++)
-            //{
-            //    outputRects[r, c] = new RectangleF(c * widthOfEachPart,
-            //     r * heightOfEachPart, widthOfEachPart, heightOfEachPart);
-            //    Bitmap splitImage = originalImage.Clone(outputRects[r, c], format);
-
-            //    splitImage.Save($"{outputFolder}/{pictureName}_r{r}c{c}.jpg");
-            //}
-
+            //Return max number of Columns, so it can be properly rendered in the UI.
+            return regularRow;
 
             //Let's try to rewrite it for any row number.
             void GenerateRowOfPictures(int r, int numberOfCols)
             {
                 float widthOfEachPart = width / numberOfCols;
-                ResultArray[r] = new string[numberOfCols];
                 for (int c = 0; c < numberOfCols; c++)
                 {
                     var myRec = new RectangleF(c * widthOfEachPart,
